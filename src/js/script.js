46c89a424c4a11bf657b1012eefe69be492b3707
MarioDev716 <mariusz.sielnicki@gmail.com>
2023-01-15 22:20:56 +0100

    Zadanie: powtórka z OOP

--- src/js/script.js
+++ src/js/script.js
@@ -1,139 +1,158 @@
 {
   ('use strict');
 
-  const select = {
-    templateOf: {
-      cartProduct: '#template-book',
-    },
-    elements: {
-      booksList: '.books-list',
-      bookImage: '.book__image',
-      filters: '.filters',
-      rating: '.book__rating__fill',
-    },
-  };
+  class BooksApp {
+    constructor() {
+      this.select = {
+        templateOf: {
+          cartProduct: '#template-book',
+        },
+        elements: {
+          booksList: '.books-list',
+          bookImage: '.book__image',
+          filters: '.filters',
+          rating: '.book__rating__fill',
+        },
+      };
 
-  const templates = {
-    cartProduct: Handlebars.compile(
-      document.querySelector(select.templateOf.cartProduct).innerHTML
-    ),
-  };
+      this.templates = {
+        cartProduct: Handlebars.compile(
+          document.querySelector(this.select.templateOf.cartProduct).innerHTML
+        ),
+      };
 
-  let favoriteBooks = [];
-  let filters = [];
+      this.settings = {
+        dbBooks: 'dataSource.books',
+      };
 
-  function productRender() {
-    const thisProduct = this;
-    const productContainer = document.querySelector(select.elements.booksList);
-    for (let book of dataSource.books) {
-      const generatedHTML = templates.cartProduct(book);
-      const thisElement = dataSource.books.indexOf(book);
-      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-      const rating = book.rating;
-      console.log(book.name + ' - ' + rating);
-      productContainer.appendChild(thisProduct.element);
-      const ratingBarDOM = document.querySelectorAll(select.elements.rating);
-      const result = prepareRatingBar(rating);
-      const ratingBarStyle = result.ratingBarStyle;
-      const widthPercentBar = result.widthPercentBar;
-      ratingBarDOM[thisElement].style.background = ratingBarStyle;
-      ratingBarDOM[thisElement].style.width = widthPercentBar + '%';
+      this.favoriteBooks = [];
+      this.filters = [];
+
+      this.initData();
+      this.productRender();
+      this.prepareRatingBar();
+      this.initActions();
     }
-  }
 
-  function prepareRatingBar(rating) {
-    let ratingBarStyle = '';
-    // Przygotowanie wartości koloru paska w zależności od oceny
-    if (rating < 6) {
-      // console.log('średnia książka');
-      ratingBarStyle = 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
-    } else if (rating > 6 && rating <= 8) {
-      // console.log('może być');
-      ratingBarStyle = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
-    } else if (rating > 8 && rating <= 9) {
-      // console.log('godna polecenia książka');
-      ratingBarStyle = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
-    } else if (rating > 9) {
-      // console.log('super książka');
-      ratingBarStyle = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
-    }
-    //Przygotowanie długości paska w zależności od oceny.
-    const widthPercentBar = rating * 10;
-    console.log(widthPercentBar + '%');
-    console.log(ratingBarStyle);
-    // ratingBarStyle += ' width: ' + widthPercentBar + '%;"';
-    //Dodawanie stylu paska do strony
-    return {ratingBarStyle: ratingBarStyle, widthPercentBar: widthPercentBar};
-  }
-
-  function initActions() {
-    document
-      .querySelector(select.elements.booksList)
-      .addEventListener('dblclick', function (event) {
-        if (event.target && event.target.matches(select.elements.bookImage)) {
-          //mam problem ze zrozumieniem dlaczego w tym miejscu nie działa mi deklaracja event.target.parentElement.classList.contains('book__image') lub event.target.parentElement.matches('.book__image').
-          //w tej chwili dwuklik wyzwala się tylko jak kliknę na ikone serca po wykonaniu hover na obrazku.
-          event.preventDefault();
-          const dataId = event.target.dataset.id;
-          if (!favoriteBooks.includes(dataId)) {
-            event.target.classList.add('favorite');
-            favoriteBooks.push(dataId);
-          } else {
-            event.target.classList.remove('favorite');
-            favoriteBooks.splice(favoriteBooks.indexOf(dataId), 1);
-          }
-          console.clear();
-        }
-      });
-    document
-      .querySelector(select.elements.filters)
-      .addEventListener('click', function (event) {
-        if (event.target.matches('input[type="checkbox"]')) {
-          const value = event.target.value;
-          if (!filters.includes(value)) {
-            filters.push(value);
-          } else {
-            filters.splice(filters.indexOf(value), 1);
-          }
-        }
-        filterAction();
-      });
-  }
-
-  function filterAction() {
-    console.clear();
-    let filterAction = [];
-    const data = dataSource.books;
-    for (let book of data) {
-      const bookImageDOM = document.querySelectorAll(select.elements.bookImage);
-      for (let filter of filters) {
-        if (book.details[filter]) {
-          if (!filterAction.includes(data.indexOf(book))) {
-            filterAction.push(data.indexOf(book));
-          }
-        }
-      }
-      if (filterAction.includes(data.indexOf(book))) {
-        bookImageDOM[data.indexOf(book)].classList.add('hidden');
-      } else {
-        bookImageDOM[data.indexOf(book)].classList.remove('hidden');
+    productRender() {
+      const books = this.books;
+      const productContainer = document.querySelector(
+        this.select.elements.booksList
+      );
+      console.log(books);
+      for (let book of books) {
+        const generatedHTML = this.templates.cartProduct(book);
+        const thisElement = books.indexOf(book);
+        this.element = utils.createDOMFromHTML(generatedHTML);
+        const rating = book.rating;
+        // console.log(book.name + ' - ' + rating);
+        productContainer.appendChild(this.element);
+        const ratingBarDOM = document.querySelectorAll(
+          this.select.elements.rating
+        );
+        const result = this.prepareRatingBar(rating);
+        const ratingBarStyle = result.ratingBarStyle;
+        const widthPercentBar = result.widthPercentBar;
+        ratingBarDOM[thisElement].style.background = ratingBarStyle;
+        ratingBarDOM[thisElement].style.width = widthPercentBar + '%';
       }
     }
+
+    prepareRatingBar(rating) {
+      let ratingBarStyle = '';
+      // Przygotowanie wartości koloru paska w zależności od oceny
+      if (rating < 6) {
+        ratingBarStyle = 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
+      } else if (rating > 6 && rating <= 8) {
+        ratingBarStyle = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+      } else if (rating > 8 && rating <= 9) {
+        ratingBarStyle = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+      } else if (rating > 9) {
+        ratingBarStyle = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+      }
+      //Przygotowanie długości paska w zależności od oceny.
+      const widthPercentBar = rating * 10;
+      // console.log(widthPercentBar + '%');
+      // console.log(ratingBarStyle);
+      //Dodawanie stylu paska do strony
+      return {
+        ratingBarStyle: ratingBarStyle,
+        widthPercentBar: widthPercentBar,
+      };
+    }
+
+    initActions() {
+      const thisApp = this;
+      document
+        .querySelector(thisApp.select.elements.booksList)
+        .addEventListener('dblclick', function (event) {
+          console.log(thisApp.select.elements.bookImage);
+          if (
+            event.target &&
+            event.target.matches(thisApp.select.elements.bookImage)
+          ) {
+            //mam problem ze zrozumieniem dlaczego w tym miejscu nie działa mi deklaracja event.target.parentElement.classList.contains('book__image') lub event.target.parentElement.matches('.book__image').
+            //w tej chwili dwuklik wyzwala się tylko jak kliknę na ikone serca po wykonaniu hover na obrazku.
+            event.preventDefault();
+            const dataId = event.target.dataset.id;
+            if (!thisApp.favoriteBooks.includes(dataId)) {
+              event.target.classList.add('favorite');
+              thisApp.favoriteBooks.push(dataId);
+            } else {
+              event.target.classList.remove('favorite');
+              thisApp.favoriteBooks.splice(
+                thisApp.favoriteBooks.indexOf(dataId),
+                1
+              );
+            }
+            console.clear();
+          }
+        });
+      document
+        .querySelector(thisApp.select.elements.filters)
+        .addEventListener('click', function (event) {
+          if (event.target.matches('input[type="checkbox"]')) {
+            const value = event.target.value;
+            if (!thisApp.filters.includes(value)) {
+              thisApp.filters.push(value);
+            } else {
+              thisApp.filters.splice(thisApp.filters.indexOf(value), 1);
+            }
+          }
+          thisApp.filterAction();
+          console.log(thisApp.filters);
+        });
+    }
+
+    filterAction() {
+      const thisApp = this;
+      const filterAction = [];
+      console.clear();
+      const books = this.books;
+      for (let book of books) {
+        const bookImageDOM = document.querySelectorAll(
+          this.select.elements.bookImage
+        );
+        for (let filter of thisApp.filters) {
+          if (book.details[filter]) {
+            if (!filterAction.includes(books.indexOf(book))) {
+              filterAction.push(books.indexOf(book));
+            }
+          }
+        }
+        if (filterAction.includes(books.indexOf(book))) {
+          bookImageDOM[books.indexOf(book)].classList.add('hidden');
+        } else {
+          bookImageDOM[books.indexOf(book)].classList.remove('hidden');
+        }
+      }
+    }
+
+    initData() {
+      const data = dataSource;
+      this.books = data.books;
+    }
   }
-
-  const app = {
-    initData: function () {
-      const thisApp = this;
-      thisApp.data = dataSource;
-    },
-
-    init: function () {
-      const thisApp = this;
-      thisApp.initData();
-      productRender();
-      initActions();
-    },
-  };
-
-  app.init();
+  const app = new BooksApp();
+  app.initData();
 }
